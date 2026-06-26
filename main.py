0a014faab689491c2da1247bf28c295af4de137c
Ganesh Sandiri <sandiriganesh1@gmail.com>
2026-06-26 16:15:44 +0530

    Added valid movement detection

--- main.py
+++ main.py
@@ -17,8 +17,35 @@ for row in grid:
         print(cell, end=" ")
     print()
 
+def get_possible_moves(grid, position):
+    row, col = position
+
+    moves = []
+
+    # Up
+    if row > 0 and grid[row - 1][col] != "X":
+        moves.append("up")
+
+    # Down
+    if row < len(grid) - 1 and grid[row + 1][col] != "X":
+        moves.append("down")
+
+    # Left
+    if col > 0 and grid[row][col - 1] != "X":
+        moves.append("left")
+
+    # Right
+    if col < len(grid[0]) - 1 and grid[row][col + 1] != "X":
+        moves.append("right")
+
+    return moves
+
 start_position = find_position(grid, "S")
 goal_position = find_position(grid, "G")
 
 print("\nRobot Position:", start_position)
 print("Goal Position:", goal_position)
+
+possible_moves = get_possible_moves(grid, start_position)
+
+print("Possible Moves:", possible_moves)
